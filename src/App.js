7f8721c58ad9dcb91c01ae20bd3e8e5afc01a441
0xc0000007b <rammleid123@gmail.com>
2023-05-20 13:02:55 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import WebApp from "@twa-dev/sdk";
+import {useEffect} from "react";
 
 function App() {
+    const tg = window.Telegram
+ useEffect(() => {
+   tg.ready()
+ })
+    const close =() => {
+        tg.close()
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <button onClick={close}>X</button>
+      </div>
     </div>
   );
 }
